f53fe12074b3ac46b1966827e94d1b94b717f6fe
FORTUNE ONOVO <onovojr@gmail.com>
2023-12-28 16:06:24 +0000

    new

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-is_hot = True
-is_cold = False
-
-if is_hot:
-    print("its a hot day")
-    print("drink water")
-elif is_cold:
-    print("its a cold day")
-    print("wear warm clothes")
-
+secret_number =9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+   guess = int( input('guess:'))
+   guess_count += 1
+   if guess ==  secret_number:
+       print('you won')
+       break
 else:
-    print("its a lovely day")
-
-print("enjoy your day")
+    print('sorry u failed')
