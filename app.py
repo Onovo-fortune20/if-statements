fe7157ea0ffb8efb171d470bc6810367ccc5fcdc
FORTUNE ONOVO <onovojr@gmail.com>
2023-12-28 13:11:04 +0000

    new

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-name = input('wat is ur name ')
-color = input('wat is your color ')
-print(name  +' likes '  + color)
+is_hot = True
+is_cold = False
+
+if is_hot:
+    print("its a hot day")
+    print("drink water")
+elif is_cold:
+    print("its a cold day")
+    print("wear warm clothes")
+
+else:
+    print("its a lovely day")
+
+print("enjoy your day")
